a085b281d52a1dd5dae3956cd7b17be4e4266f87
Wahid Rahim <wahidrahim@gmail.com>
2020-03-19 00:12:31 -0400

    make grid component

---
+++ src/components/grid/grid.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './grid.styles.scss'
+
+const Grid = () => {
+  return (
+    <div className="grid">
+      <div className="cell">1</div>
+      <div className="cell">2</div>
+    </div>
+  )
+}
+
+export default Grid
