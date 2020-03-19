6b1649bfb47aa92ae982e24a1e0f25eaa3cd66ef
Wahid Rahim <wahidrahim@gmail.com>
2020-03-19 00:59:46 -0400

    validate Grid component props
    - props must have children that are renderable nodes
    - props must have a number of children that is has an integer square root

--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropType from 'prop-types'
 
 import './grid.styles.scss'
 
@@ -8,11 +9,11 @@ const Grid = ({ children }) => {
     height: 300,
     get gridTemplateColumns() {
       const numberOfCells = children.length
-      const gridSize = numberOfCells / 2
+      const gridSize = numberOfCells / Math.sqrt(numberOfCells)
       let columns = ''
 
       for (let i = 0; i < gridSize; i++) {
-        columns += `${100 / gridSize}%`
+        columns += `${100 / gridSize}%` // '50%50%', '25%25%25%25%', etc.
       }
 
       return columns
@@ -22,9 +23,28 @@ const Grid = ({ children }) => {
     }
   }
 
-  return <div className="grid" style={gridStyles}>
-    {children.map(cell => cell)} 
-  </div>
+  return (
+    <div className="grid" style={gridStyles}>
+      {children.map(cell => cell)}
+    </div>
+  )
+}
+
+Grid.propTypes = {
+  children: props => {
+    const hasIntegerSquareRoot = number => Number.isInteger(Math.sqrt(number))
+
+    if (!hasIntegerSquareRoot) {
+      throw new Error('not squareable grid')
+    }
+
+    PropType.checkPropTypes(
+      {
+        children: PropType.arrayOf(PropType.node)
+      },
+      props
+    )
+  }
 }
 
 export default Grid
