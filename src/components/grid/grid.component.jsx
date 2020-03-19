39a354a4abad7be7c287dc38b170c0f968b248c0
Wahid Rahim <wahidrahim@gmail.com>
2020-03-19 00:16:43 -0400

    update grid styles

--- src/components/grid/grid.component.jsx
+++ src/components/grid/grid.component.jsx
@@ -3,10 +3,19 @@ import React from 'react'
 import './grid.styles.scss'
 
 const Grid = () => {
+  const gridStyle = {
+    width: 300,
+    height: 300,
+    gridTemplateColumns: '50%50%',
+    gridTemplateRows: '50%50%',
+  }
+
   return (
-    <div className="grid">
+    <div className="grid" style={gridStyle}>
       <div className="cell">1</div>
       <div className="cell">2</div>
+      <div className="cell">3</div>
+      <div className="cell">4</div>
     </div>
   )
 }
