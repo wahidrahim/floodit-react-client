5b508e3733b2de3649bf2d1863971c7e77ceb9f2
Wahid Rahim <wahidrahim@gmail.com>
2020-03-21 01:51:02 -0400

    update style

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ const getRandomColor = () => {
   const getRandomIntInclusive = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
-  
+
   const colors = [
-    '#FF00E7',
-    '#FF8100',
-    '#E3FF00',
-    '#00FF57',
-    '#00C5FF',
-    '#A300FF'
+    '#ff00e7',
+    '#ff8100',
+    '#e3ff00',
+    '#00ff57',
+    '#00c5ff',
+    '#a300ff'
   ]
   const randomIndex = getRandomIntInclusive(0, colors.length - 1)
 
